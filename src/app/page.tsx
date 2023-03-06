52d51f52f798a56329aa1b6f770be4400c1faf91
ExoskyCode <exoskyv2@gmail.com>
2023-03-06 21:48:31 +0100

    add Blog UI, db schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
+import { AddBlog } from "./components/Blog/AddBlog/AddBlog"
+
+
 export default function Home() {
   return (
     <div className="p-4">
-      <h1 className="text-4xl text-white">Home</h1>
+      <h1 className="text-2xl text-white">Mon blog</h1>
+      <AddBlog />
     </div>
   )
 }
