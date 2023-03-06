838845377c0375a547ed9d156cec71ac5d89e1e1
ExoskyCode <exoskyv2@gmail.com>
2023-03-06 21:12:15 +0100

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="text-4xl">Home</h1>
+    <div className="p-4">
+      <h1 className="text-4xl text-white">Home</h1>
+    </div>
   )
 }
